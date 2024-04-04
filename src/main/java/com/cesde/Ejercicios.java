f6d96d4e4320b8e0b9575cf33c9093ec6acc5aca
Kevin Olivella <kevinolivella@gmail.com>
2024-04-03 22:10:33 -0500

    cambio1

--- src/main/java/com/cesde/Ejercicios.java
+++ src/main/java/com/cesde/Ejercicios.java
@@ -3,8 +3,14 @@ package com.cesde;
 public class Ejercicios {
     
     public String ejercicio1(int num1, int num2) {
-        // Implementar solución
-        return "";
+        if (num1>num2) {
+            return "El número mayor es: num1";
+        } else if (num2>num1) {
+            return "El número mayor es: num2";
+        } else {
+            return "Los números son iguales.";
+        }
+        
     }
     
     public String ejercicio2(double calificacion) {
